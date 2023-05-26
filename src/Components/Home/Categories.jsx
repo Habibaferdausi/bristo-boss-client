c715ea0f490ec4831de7e71ae60e6cd314cd4ca9
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-26 20:58:29 +0600

    add categoried

--- src/Components/Home/Categories.jsx
+++ src/Components/Home/Categories.jsx
@@ -2,15 +2,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
+import SectionTitle from "./SectionTitle";
 
 const Categories = () => {
   return (
     <div>
-      <section>
-        <h1
+      <section className="mt-10">
+        <SectionTitle
           subHeading={"From 11.00am to 10.00pm"}
           heading={"Order Online"}
-        ></h1>
+        ></SectionTitle>
         <Swiper
           slidesPerView={4}
           spaceBetween={30}
@@ -19,33 +20,56 @@ const Categories = () => {
             clickable: true,
           }}
           modules={[Pagination]}
-          className="mySwiper mb-24"
+          className="mySwiper mt-11 mb-24"
         >
           <SwiperSlide>
             <img
-              src="https://plus.unsplash.com/premium_photo-1663852297801-d277b7af6594?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              src="https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
+              className="h-64"
             />
-            <h3 className="text-4xl uppercase text-center -mt-16 text-white">
-              Salads
-            </h3>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"
-              alt=""
-            />
-            <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            <h3 className="text-4xl uppercase text-center -mt-16 text-white shadow-xl">
               Pizzas
             </h3>
           </SwiperSlide>
           <SwiperSlide>
             <img
-              src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              src="https://images.pexels.com/photos/688802/pexels-photo-688802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
+              className="h-64 "
             />
-            <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            <h3 className="text-4xl uppercase text-center -mt-16 text-white shadow-xl">
               Soups
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://images.pexels.com/photos/5038075/pexels-photo-5038075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="h-64"
+            />
+            <h3 className="text-4xl uppercase text-center -mt-16 text-white shadow-xl">
+              Desserts
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="h-64"
+            />
+            <h3 className="text-4xl uppercase text-center -mt-16 text-white shadow-xl">
+              Pasta
+            </h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://images.pexels.com/photos/3085148/pexels-photo-3085148.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+              className="h-64"
+            />
+            <h3 className="text-4xl uppercase text-center -mt-16 text-white shadow-xl">
+              Salads
             </h3>
           </SwiperSlide>
         </Swiper>
---
+++ src/Components/Home/PopularMenu.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
+  return <div></div>;
+};
+
+export default PopularMenu;
