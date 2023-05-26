ae6f3d63c20e2842cc5ff29690c84cd4647f94a9
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-27 01:11:27 +0600

    add menu categories

--- src/Components/Menu.jsx/Menu.jsx
+++ src/Components/Menu.jsx/Menu.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover";
-
+import MenuCategory from "./MenuCategory";
+import useMenu from "../../Hooks/useMenu";
+import SectionTitle from "../Home/SectionTitle";
 const Menu = () => {
-  // const [menu] = useMenu();
+  const [menu] = useMenu();
   const desserts = menu.filter((item) => item.category === "dessert");
   const soup = menu.filter((item) => item.category === "soup");
   const salad = menu.filter((item) => item.category === "salad");
@@ -30,21 +32,24 @@ const Menu = () => {
         <MenuCategory
           items={desserts}
           title="dessert"
-          img={dessertImg}
+          img="https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         ></MenuCategory>
         <MenuCategory
           items={pizza}
           title={"pizza"}
-          img={pizzaImg}
+          img="https://images.pexels.com/photos/10790638/pexels-photo-10790638.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         ></MenuCategory>
         <MenuCategory
           items={salad}
           title={"salad"}
-          img={saladImg}
+          img="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         ></MenuCategory>
-        <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
+        <MenuCategory
+          items={soup}
+          title={"soup"}
+          img="https://images.pexels.com/photos/5865282/pexels-photo-5865282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        ></MenuCategory>
       </div>
-      );
     </div>
   );
 };
